7bbbbc1767cc4550567487108888d5a0d35e81af
hirokinishizawa <hiroki.nishizawa@scouter.co.jp>
2019-02-07 00:12:46 +0900

    logoutページ

---
+++ pages/logout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div/>
+</template>
+
+<script>
+import { unsetAuthData } from '~/utils/auth'
+
+export default {
+  layout: 'noScroll',
+  head() {
+    return {
+      title: 'ログアウト'
+    }
+  },
+  created() {
+    unsetAuthData()
+    this.$store.commit('auth/removeToken')
+    this.$store.commit('user/remove')
+    window.location.replace(this.$router.resolve({ name: 'login' }).href)
+  }
+}
+</script>
